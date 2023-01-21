ae9038157e5d621471b8884727d317d2d2c10bd9
lakshyakalia <lkalia.cse16@chitkarauniversity.edu.in>
2023-01-21 13:58:46 -0500

    Added error labels functionality

---
+++ password-generator.js
@@ -0,0 +1,6 @@
+function showLabels(){
+        document.getElementById("label1").style.display = "block";
+        document.getElementById("label2").style.display = "block";
+        document.getElementById("label3").style.display = "block";
+        document.getElementById("label4").style.display = "block";
+}
